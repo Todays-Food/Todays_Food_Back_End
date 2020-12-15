764d11b401602233a585e1c371d01b1038beda2e
vreez <dearsoou@gmail.com>
2020-12-16 03:01:17 +0900

    accounts django 작성

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_201_CREATED
+) 
+from .serializers import UserSerializer
 
-# Create your views here.
-def index(request):
-    pass
+
+@api_view(['POST'])
+def signup(request):
+    password = request.data.get('password')
+    password_confirmation = request.data.get('passwordConfirmation')
+
+    if password != password_confirmation:
+        return Response({'error': '비밀번호가 일치하지 않습니다.'}, status=HTTP_400_BAD_REQUEST)
+
+    serializer = UserSerializer(data=request.data) 
+    if serializer.is_valid(raise_exception=True):
+        user = serializer.save()
+        user.set_password(password)
+        user.save()
+        return Response(serializer.data, status=HTTP_201_CREATED)   
