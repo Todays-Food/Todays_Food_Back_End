746880d55ed771977400f3e55924d43de3dcb4d9
vreez <dearsoou@gmail.com>
2020-12-16 23:27:12 +0900

    signup, login 로직 확인작업

--- accounts/views.py
+++ accounts/views.py
@@ -10,14 +10,18 @@ from .serializers import UserSerializer
 
 @api_view(['POST'])
 def signup(request):
+    print('여기는 들어왔나?')
     password = request.data.get('password')
     password_confirmation = request.data.get('passwordConfirmation')
-
+    print(password)
+    print(password_confirmation)
     if password != password_confirmation:
+        print('일치하지 않습니다!!')
         return Response({'error': '비밀번호가 일치하지 않습니다.'}, status=HTTP_400_BAD_REQUEST)
 
     serializer = UserSerializer(data=request.data) 
     if serializer.is_valid(raise_exception=True):
+        print('여기로 왔나??')
         user = serializer.save()
         user.set_password(password)
         user.save()
